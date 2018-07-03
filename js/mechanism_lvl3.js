7c2f984524d6dccfda145c8e842844eabac9fe84
kosek <kosek@github.com>
2018-07-03 18:02:40 +0200

    Fix bugs
    Change with bug on mobile

--- js/mechanism_lvl3.js
+++ js/mechanism_lvl3.js
@@ -44,7 +44,8 @@ const clickedCard = function()
 
                                                setTimeout( () =>
                                              {
-                                                   document.querySelector(".summary-level-three").style.display = "flex";
+                                                   document.querySelector(".container-level-four").style.display = "none";
+                                                   document.querySelector(".summary-level-four").style.display = "block";
                                                    document.querySelector(".flash-card").style.display = "none";
                                                    document.querySelector("body").style.backgroundColor = "black";
                                                    document.querySelector(".previous").addEventListener("click", () => location.href = "level2.html");
