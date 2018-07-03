182e294951e63e5c61ee4cb7bcee2d024c47d283
kosek <kosek@github.com>
2018-07-03 17:19:38 +0200

    Added lvl5

---
+++ js/mechanism_lvl5.js
@@ -0,0 +1,93 @@
+const cardsTab = ["cat", "cat-word", "dog", "dog-word", "cow", "cow-word", "tiger", "tiger-word" , "lion", "lion-word", "panda", "panda-word", "fox", "fox-word", "elephant",
+                                 "elephant-word", "fish", "fish-word", "ladybird", "ladybird-word"];
+
+let  cards = document.querySelectorAll(".flash-card div");
+cards = [... cards];
+
+let activeCard = "";
+const activeCards = [];
+
+ const cardPairs = 10;
+ let gameLength = 0;
+
+const clickedCard = function()
+{
+    activeCard = this;
+    if(activeCards[0] == activeCard) return;
+    activeCard.classList.remove("hidden");
+
+    if(activeCards.length === 0)
+    {
+       activeCards[0] = activeCard;
+    }
+
+    else
+    {
+           activeCards[1] = activeCard;
+           cards.forEach( e => e.removeEventListener("click", clickedCard) );
+
+           setTimeout( () =>
+             {
+                   if(activeCards[0].className === "cat" && activeCards[1].className === "cat-word" || activeCards[0].className === "cat-word" && activeCards[1].className === "cat" ||
+                       activeCards[0].className === "dog" && activeCards[1].className === "dog-word" || activeCards[0].className === "dog-word" && activeCards[1].className === "dog" ||
+                       activeCards[0].className === "cow" && activeCards[1].className === "cow-word" || activeCards[0].className === "cow-word" && activeCards[1].className === "cow" ||
+                       activeCards[0].className === "tiger" && activeCards[1].className === "tiger-word" || activeCards[0].className === "tiger-word" && activeCards[1].className === "tiger" ||
+                       activeCards[0].className === "lion" && activeCards[1].className === "lion-word" || activeCards[0].className === "lion-word" && activeCards[1].className === "lion" ||
+                       activeCards[0].className === "panda" && activeCards[1].className === "panda-word" || activeCards[0].className === "panda-word" && activeCards[1].className === "panda" ||
+                       activeCards[0].className === "fox" && activeCards[1].className === "fox-word" || activeCards[0].className === "fox-word" && activeCards[1].className === "fox" ||
+                       activeCards[0].className === "elephant" && activeCards[1].className === "elephant-word" || activeCards[0].className === "elephant-word" && activeCards[1].className === "elephant" ||
+                       activeCards[0].className === "fish" && activeCards[1].className === "fish-word" || activeCards[0].className === "fish-word" && activeCards[1].className === "fish" ||
+                       activeCards[0].className === "ladybird" && activeCards[1].className === "ladybird-word" || activeCards[0].className === "ladybird-word" && activeCards[1].className === "ladybird")
+                         {
+                             activeCards.forEach( e => e.classList.add("off"));
+                             gameLength++;
+
+                                   if(gameLength == cardPairs)
+                                   {
+                                               document.querySelector(".flash-card").style.border = "0";
+                                               cards.forEach( e =>  e.style.border = "0")
+
+                                               setTimeout( () =>
+                                             {
+                                                   document.querySelector(".container-level-five").style.display = "none";
+                                                   document.querySelector(".summary-level-five").style.display = "flex";
+                                                   document.querySelector("body").style.backgroundColor = "black";
+                                                   document.querySelector(".previous").addEventListener("click", () => location.href = "level4.html");
+                                                   document.querySelector(".again").addEventListener("click", () => location.reload() );
+                                                   document.querySelector(".next").addEventListener("click", () => location.href= "level6.html");
+                                             }, 250)
+                                     }
+                         }
+
+                    else
+                          {
+                            activeCards.forEach( e => e.classList.add("hidden"));
+                          }
+
+                    activeCard = "";
+                    activeCards.length = 0;
+                    cards.forEach( e => e.addEventListener("click", clickedCard))
+             }, 500)
+      }
+
+
+
+}
+
+const randomCards = () =>
+{
+     cards.forEach(e =>
+     {
+         const individualPosition = Math.floor(Math.random() * cardsTab.length);
+         e.classList.add(cardsTab[individualPosition]);
+         cardsTab.splice(individualPosition, 1);
+     })
+
+          setTimeout( () =>
+          {
+             cards.forEach(e => e.classList.add("hidden"));
+             cards.forEach(e => e.addEventListener("click", clickedCard));
+          }, 7000)
+}
+
+randomCards();
