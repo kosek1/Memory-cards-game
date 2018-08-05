a05c26b7983f5f2c82331266cbb0f0e172435d43
kosek <kosek@github.com>
2018-08-05 23:32:42 +0200

    2.04

--- js/change_level.js
+++ js/change_level.js
@@ -1,15 +1,7 @@
-const button = document.querySelector(".choose-level");
-const windowLevels = document.querySelector(".window-levels-off");
+const button = document.querySelector(".button-level");
+const levelList = document.querySelector(".level-list");
 
-const level = [...document.querySelectorAll(".level-button")];
 
-button.addEventListener("click", function()
-{
-       windowLevels.classList.remove("window-levels-off")
-        windowLevels.classList.toggle("window-levels-on");
-
-        level.forEach(function(e)
-      {
-          e.classList.toggle("level-button-on");
-      })
-});
+button.addEventListener("click", ()=> {
+   levelList.classList.toggle("active-flex")
+})
