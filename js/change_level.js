b3710caf13d964dc7493d430f119f9f04f6b00c2
kosek <kosek@github.com>
2018-08-30 14:07:46 +0200

    2.3.3
    -names
    -added topbar
    -changed position timer
    - change menu levels on mobile

--- js/change_level.js
+++ js/change_level.js
@@ -1,7 +1,25 @@
-const button = document.querySelector(".button-level");
+const button = [...document.querySelectorAll(".my-topbar-buttons")];
 const levelList = document.querySelector(".level-list");
 
+const word = [...document.querySelectorAll(".my-topbar-buttons span")];
 
-button.addEventListener("click", ()=> {
-   levelList.classList.toggle("active-flex")
-})
+let toggled = false;
+
+
+button.forEach( (e)=> {
+    e.addEventListener("click", ()=> {
+       levelList.classList.toggle("active-flex");
+       toggled = !toggled;
+
+       if(toggled == true) {
+          word.forEach((e)=> {
+              if(window.innerWidth >= 768) {e.textContent = "Ukryj";}
+              else {e.classList = "fa fa-times"}
+          }) }
+        else {
+            word.forEach((e)=> {
+              if(window.innerWidth >= 768) {e.textContent = "Level";}
+              else {e.classList = "fa fa-bars"}
+            }) }
+    });
+});
